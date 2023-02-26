b190e30d2be86dd24e1634eaae1243e01b889796
YUNGCHIN77 <daiqin.forever@gmail.com>
2023-02-26 16:02:38 +0800

    after filtering

--- read.py
+++ read.py
@@ -1,6 +1,7 @@
 data = []
 count = 0
 length = 0
+new = []
 
 with open("reviews.txt", "r") as f:
     for line in f:
@@ -9,9 +10,14 @@ with open("reviews.txt", "r") as f:
         count += 1
         if count % 1000 == 0:
             print(len(data))
+                      
+    for line in data:
+        if len(line) < 100:
+            new.append(line)
+               
             
 print(f"檔案讀取完了,總共有{len(data)}筆資料")            
 average = length/len(data)            
 print("留言的平均長度=", average)
 
-
+print(f"一共有{len(new)}筆留言長度小於100")    
